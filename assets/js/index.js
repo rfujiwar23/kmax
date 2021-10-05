2d7f985f198f82fcbedd45609590e07038c04b60
Ryo Fujiwara <jjcproject@icloud.com>
2021-10-05 14:57:32 +0900

    aaaaaa

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,9 +18,9 @@ var vm = new Vue({
     data: {
       pictureList: [
        {imgUrl: 'assets/images/kmx012.png', text: 'デジタルの可能性を', text2: '最大に活用', alt: 'KAMISMAX 画像　カメラ'},
-       {imgUrl: 'assets/images/kmx013.png', text: '圧倒的なクオリティで見る', text2: '鮮明な映像', alt: 'KAMISMAX 画像　カメラ'},
+       {imgUrl: 'assets/images/kmx015.png', text: '圧倒的なクオリティで見る', text2: '鮮明な映像', alt: 'KAMISMAX 画像　カメラ'},
        {imgUrl: 'assets/images/kmx014.png', text: 'マルチアングル（多視点）で', text2: '見たい視点が選択可能', alt: 'KAMISMAX 画像　カメラ'},
-       {imgUrl: 'assets/images/kmx015.png', text: '髪の毛１本１本を', text2: '鮮明に捉える4Kカメラで撮影', alt: 'KAMISMAX 画像　カメラ'},
+       {imgUrl: 'assets/images/kmx013.png', text: '髪の毛１本１本を', text2: '鮮明に捉える4Kカメラで撮影', alt: 'KAMISMAX 画像　カメラ'},
 
       ]
     }
@@ -72,7 +72,7 @@ var vm = new Vue({
      {imgUrl:'assets/images/kmx007.png', body: 'インタビューや密着など「リアルを配信」',  title: 'リアリティ'},
      {imgUrl:'assets/images/kmx011.png', body: '色々な企画の対談やバトルなどを配信',  title: 'バラエティ'},
      {imgUrl:'assets/images/kmx008.png', body: 'ヘアショーやイベント映像を配信',  title: 'エンターテインメント'},
-     {imgUrl:'assets/images/kmx009.png', body: '感性を刺激する特別な映像っを配信',  title: 'スペシャル'},
+     {imgUrl:'assets/images/kmx009.png', body: '感性を刺激する特別な映像を配信',  title: 'スペシャル'},
      {imgUrl:'assets/images/kmx010.png', body: 'KAMI CHARISMAの授賞式等の関連画像を紹介',  title: 'カミカリスマ'},
     ]
   }
